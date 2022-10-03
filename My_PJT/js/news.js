f5f685a83d7240ce0bf9408ffe883c724523f859
Hanbeul You <109324631+hanbeulYou@users.noreply.github.com>
2022-10-03 18:24:04 +0900

    news 기능 구현

--- My_PJT/js/news.js
+++ My_PJT/js/news.js
@@ -1,1 +1,22 @@
-const NEWS_API = globalVarible.NEWS_API;
+const NEWS_API = globalVariable.NEWS_API;
+
+var newsURL = 'https://newsapi.org/v2/top-headlines?' + 'country=kr&' + `apiKey=${NEWS_API}`;
+var req = new Request(newsURL);
+
+fetch(req)
+    .then(response => response.json())
+    .then(data => {
+        const newsContent = document.querySelector(".news__content");
+        const newsList = document.createElement("ul");
+        newsContent.appendChild(newsList);
+        
+        const articleOne = document.createElement("li");
+        newsList.appendChild(articleOne);
+        articleOne.innerText = data.articles[0].title;
+        const articleTwo = document.createElement("li");
+        newsList.appendChild(articleTwo);
+        articleTwo.innerText = data.articles[1].title;
+        const articleThree = document.createElement("li");
+        newsList.appendChild(articleThree);
+        articleThree.innerText = data.articles[2].title;
+    })
