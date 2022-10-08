403d620c1a7f82b184db4433bb10173167c14b3b
Hanbeul You <109324631+hanbeulYou@users.noreply.github.com>
2022-10-08 16:32:46 +0900

    music player 기능 구현

--- My_PJT/js/player.js
+++ My_PJT/js/player.js
@@ -3,7 +3,22 @@ tag.src = 'https://www.youtube.com/iframe_api';
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-const musicID = [
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '200',
+        width: '400',
+        videoId: musicID[0].id,
+        events: {
+            'onReady': function(event){
+                event.target.playVideo();
+            },
+            'onStateChange': onPlayerStateChange
+        },
+    });
+}
+
+let musicID = [
     {
         title: '밤하늘의 별을...',
         musician: '양정승',
@@ -44,34 +59,90 @@ const musicID = [
         musician: '윤하',
         id: 'iB3VDOQPvi0',
     },
+    {
+        title: '비밀번호 486',
+        musician: '윤하',
+        id: 'RNqXM7T7f_A'
+    },
+    {
+        title: '비밀번호 486',
+        musician: '윤하',
+        id: 'RNqXM7T7f_A',
+    },
+    {
+        title: '가시',
+        musician: '버즈',
+        id: 'AoVt7Qe9t1s',
+    },
 ]
-const randomMusic = musicID[Math.floor(Math.random()*musicID.length)]
 
-var player;
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '200',
-        width: '400',
-        videoId: randomMusic.id,
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange,
-        }
-    });
+// 리스트 셔플
+function shuffleList(array) {
+    array.sort(() => Math.random() - 0.5);
 }
 
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
+// 셔플된 음악리스트
+// const randomMusic = musicID[Math.floor(Math.random()*musicID.length)]
+shuffleList(musicID);
+const title = document.querySelector(".player__title")
 
-var done = false;
 function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 000000);
-        done = true;
+    if(event.data == YT.PlayerState.ENDED) {
+        handleNextBtnClick();
+        event.target.playVideo();
     }
 }
 
-function stopVideo() {
+var pause = false;
+function handlePauseBtnClick() {
+    console.log("PAUSE Button")
+    title.innerText = `${musicID[nowPlaying].title} - ${musicID[nowPlaying].musician}`;
+    if(pause === false) {
+        player.playVideo();
+        pause = true;
+        pauseButton.innerText = '⏸';
+    } else {
+        player.pauseVideo();
+        pause = false;
+        pauseButton.innerText = '▶';
+    }
+}
+
+let nowPlaying = 0;
+function handleNextBtnClick() {
+    console.log("NEXT Button");
+    if(nowPlaying >= musicID.length-1) {
+        nowPlaying = 0;
+    } else {
+        nowPlaying++;
+    }
+    title.innerText = `${musicID[nowPlaying].title} - ${musicID[nowPlaying].musician}`;
+    player.loadVideoById(musicID[nowPlaying].id)
+}
+
+function handleBeforeBtnClick() {
+    console.log("BEFORE Button");
+    if(nowPlaying <= 0) {
+        nowPlaying = musicID.length-1;
+    } else {
+        nowPlaying--;
+    }
+    title.innerText = `${musicID[nowPlaying].title} - ${musicID[nowPlaying].musician}`;
+    player.loadVideoById(musicID[nowPlaying].id)
+}
+
+function handleStopBtnClick() {
+    title.innerText = '';
+    console.log("STOP Button")
     player.stopVideo();
 }
+
+const beforeButton = document.querySelector("#before")
+const pauseButton = document.querySelector("#pause")
+const stopButton = document.querySelector("#stop")
+const nextButton = document.querySelector("#next")
+
+beforeButton.addEventListener("click", handleBeforeBtnClick)
+pauseButton.addEventListener("click", handlePauseBtnClick)
+stopButton.addEventListener("click", handleStopBtnClick)
+nextButton.addEventListener("click", handleNextBtnClick)
